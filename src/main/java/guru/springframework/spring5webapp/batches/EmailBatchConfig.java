4262cb32b344ba1fed293aca80c94e2dd39ddc6f
BSimham <57747814+BSimham@users.noreply.github.com>
2023-04-10 22:13:41 +0530

    changes

--- src/main/java/guru/springframework/spring5webapp/batches/EmailBatchConfig.java
+++ src/main/java/guru/springframework/spring5webapp/batches/EmailBatchConfig.java
@@ -2,6 +2,7 @@ package guru.springframework.spring5webapp.batches;
 
 import java.util.List;
 
+import javax.batch.api.chunk.listener.ChunkListener;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.batch.core.Job;
@@ -11,7 +12,9 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.listener.ChunkListenerSupport;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
@@ -51,15 +54,41 @@ public class EmailBatchConfig  {
                 .start(emailStep())
                 .build();
     }
-
-
+    
     @Bean
+    public ChunkListenerSupport chunkListener() {
+    	return new ChunkListenerSupport() {
+			
+//			@Override
+//			public void onError(Exception ex) throws Exception {
+//				System.out.println("EmailJob.ErrorChunk");
+//				
+//			}
+			
+			@Override
+			public void beforeChunk(ChunkContext context) {
+				System.out.println("EmailJob.beforeChunk");
+    		
+				
+			}
+			
+			@Override
+			public void afterChunk(ChunkContext context) {
+				System.out.println("EmailJob.afterChunk");
+				
+			}
+    };
+   }
+
+
+@Bean
     public Step emailStep() {
         return stepBuilderFactory.get("emailStep")
-        		.<List<Email>,List<MimeMessage>>chunk(10)
+        		.<List<Email>,List<MimeMessage>>chunk(2)
                 .reader(emailMessageReader)
                 .processor(emailMessageProcessor)
                 .writer(emailMessageSender)
+                .listener(chunkListener())
                 .build();
     }
     
