17b4014f88ae5ff6e67deb37859baf1e4ac2e4b8
BSimham <57747814+BSimham@users.noreply.github.com>
2023-04-10 22:07:51 +0530

    changes

--- src/main/java/guru/springframework/spring5webapp/repositories/EmailRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/EmailRepository.java
@@ -1,10 +1,23 @@
 package guru.springframework.spring5webapp.repositories;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import guru.springframework.spring5webapp.domain.Email;
 
 
 import guru.springframework.spring5webapp.domain.Email;
 
 public interface EmailRepository extends JpaRepository<Email, Long> {
 
+  @Query(
+			value = "SELECT e FROM Email e WHERE e.flag= false"	,
+			countQuery = "SELECT COUNT(e) FROM Email e WHERE e.flag= false"
+
+		)
+	  Page<Email> findAllFalse(Pageable pageable);
 }
